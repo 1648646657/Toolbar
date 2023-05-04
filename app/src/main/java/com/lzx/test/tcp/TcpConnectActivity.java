e908bce42b2c8cf0630c7ad5f88fc013b6f952a7
lzx <1648646657@qq.com>
2023-05-04 17:54:01 +0800

    [Style] 上次提交合并出错

--- app/src/main/java/com/lzx/test/tcp/TcpConnectActivity.java
+++ app/src/main/java/com/lzx/test/tcp/TcpConnectActivity.java
@@ -6,10 +6,9 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
-<<<<<<< HEAD
+
 import android.util.Log;
-=======
->>>>>>> github/master
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,10 +21,7 @@ public class TcpConnectActivity extends AppCompatActivity {
 
     private TcpServerThread mServerThread;
     private TcpClientThread mClientThread;
-<<<<<<< HEAD
     private Handler mHandler;
-=======
->>>>>>> github/master
     private EditText mEditText;
     private Button mClientSendBtn;
     private Button mServerSendBtn;
@@ -33,7 +29,6 @@ public class TcpConnectActivity extends AppCompatActivity {
     private Button mCloseBtn;
     private TextView mServerTextView;
     private TextView mClientTextView;
-<<<<<<< HEAD
     private static final String address = "192.168.1.221";
 //    private String address = "10.13.5.131";
 //    private String address = "192.168.75.83";
@@ -43,19 +38,10 @@ public class TcpConnectActivity extends AppCompatActivity {
     private static final String oneToOne = "SET IN1 VIDEO OUT1\r\n";
     private static final String twoToOne = "SET IN2 VIDEO OUT1\r\n";
 
-=======
-    private String address = "192.168.10.12";
-//    private String address = "10.13.5.131";
-//    private String address = "192.168.75.83";
-    private int port = 12345;
-    private boolean isOpen = false;
-
->>>>>>> github/master
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tcp_connect);
-
         mEditText = findViewById(R.id.edit_text);
         mClientSendBtn = findViewById(R.id.client_send_btn);
         mServerSendBtn = findViewById(R.id.server_send_btn);
@@ -63,7 +49,6 @@ public class TcpConnectActivity extends AppCompatActivity {
         mCloseBtn = findViewById(R.id.close_btn);
         mServerTextView = findViewById(R.id.server_receive_text);
         mClientTextView = findViewById(R.id.client_receive_text);
-<<<<<<< HEAD
 //        mServerSendBtn.setVisibility(View.GONE);
         mEditText.setVisibility(View.GONE);
         mOpenBtn.setVisibility(View.GONE);
@@ -90,19 +75,16 @@ public class TcpConnectActivity extends AppCompatActivity {
                 }
             }
         };
-=======
->>>>>>> github/master
 
         mClientSendBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-<<<<<<< HEAD
-                if(true){
+                if (true) {
 //                    if(mClientThread != null){
-                    if(false){
+                    if (false) {
                         mClientThread.stoped();
                         //客户端断开后服务端需重新连接
-                        if(mServerThread != null){
+                        if (mServerThread != null) {
                             mServerThread.stoped();
                             mServerThread = new TcpServerThread(false, true, mEditText.getText().toString(), mHandler);
                             mServerThread.start();
@@ -110,31 +92,29 @@ public class TcpConnectActivity extends AppCompatActivity {
                         mClientThread = new TcpClientThread(true, false, address, port, mEditText.getText().toString(), mHandler);
                         mClientThread.start();
                         Log.d("lzx", "onClick: if");
-                    }else {
-                        if(mClientThread != null){
+                    } else {
+                        if (mClientThread != null) {
                             mClientThread.stoped();
                             mClientThread = new TcpClientThread(true, false,
                                     address, port, oneToOne, mHandler);
                             mClientThread.start();
-                        }else {
+                        } else {
                             mClientThread = new TcpClientThread(true, false,
                                     address, port, oneToOne, mHandler);
                             mClientThread.start();
                         }
-                        Toast.makeText(TcpConnectActivity.this,"发送1切1",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(TcpConnectActivity.this, "发送1切1", Toast.LENGTH_SHORT).show();
                     }
-=======
-                if(isOpen){
-                    mClientThread = new TcpClientThread(true, false, address, port, mEditText.getText().toString(), mHandler);
-                    mClientThread.start();
->>>>>>> github/master
+                    if (isOpen) {
+                        mClientThread = new TcpClientThread(true, false, address, port, mEditText.getText().toString(), mHandler);
+                        mClientThread.start();
+                    }
                 }
             }
         });
         mServerSendBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-<<<<<<< HEAD
 
                 if(mClientThread != null){
                     mClientThread.stoped();
@@ -176,7 +156,6 @@ public class TcpConnectActivity extends AppCompatActivity {
 ////                    Log.d("lzx", "mServerSendBtn.onClick: mClientThread == null");
 //                    mServerThread = new TcpServerThread(true, false, mEditText.getText().toString(), mHandler);
 //                    mServerThread.start();
-=======
                 if(isOpen){
 //                    if(mClientThread == null){
 //                        mClientThread = new TcpClientThread(false, true, address, port, mEditText.getText().toString(), mHandler);
@@ -186,7 +165,6 @@ public class TcpConnectActivity extends AppCompatActivity {
 //                    mServerThread = new TcpServerThread(true, true, mEditText.getText().toString(), mHandler);
 //                    mServerThread.start();
                 }
->>>>>>> github/master
             }
         });
         mOpenBtn.setOnClickListener(new View.OnClickListener() {
@@ -194,17 +172,14 @@ public class TcpConnectActivity extends AppCompatActivity {
             public void onClick(View v) {
                 isOpen = true;
                 try {
-<<<<<<< HEAD
                     if(mServerThread == null){
                         mServerThread = new TcpServerThread(false, true, mEditText.getText().toString(), mHandler);
                         mServerThread.start();
                     }
 //                    mClientThread = new TcpClientThread(false, true, address, port, mEditText.getText().toString(), mHandler);
 //                    mClientThread.start();
-=======
                     mServerThread = new TcpServerThread(false, true, mEditText.getText().toString(), mHandler);
                     mServerThread.start();
->>>>>>> github/master
                     Toast.makeText(TcpConnectActivity.this,"已开启服务",Toast.LENGTH_SHORT).show();
 //                    TcpClientThread mClientThread = new TcpClientThread(false, address, port, mEditText.getText().toString(), mHandler);
 //                    mClientThread.start();
@@ -232,45 +207,28 @@ public class TcpConnectActivity extends AppCompatActivity {
         });
     }
 
-<<<<<<< HEAD
-=======
-    private Handler mHandler = new Handler(Looper.myLooper()) {
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            switch (msg.what) {
-                case 0:
-                    String mClientcontent= (String) msg.obj;
-                    mClientTextView.setText(mClientcontent);
-                    break;
-                case 1:
-                    String mServerContent= (String) msg.obj;
-                    mServerTextView.setText(mServerContent);
-                    break;
-
-            }
-        }
-    };
-
-
->>>>>>> github/master
-    @Override
-    protected void onResume() {
-        super.onResume();
-//        mServerThread = new TcpServerThread(false);
-//        mServerThread.start();
-//        if(mServerThread == null){
-//            mServerThread = new TcpServerThread(false, mEditText.getText().toString(), mHandler);
-//            mServerThread.start();
+//    private Handler mHandler = new Handler(Looper.myLooper()) {
+//        @Override
+//        public void handleMessage(Message msg) {
+//            super.handleMessage(msg);
+//            switch (msg.what) {
+//                case 0:
+//                    String mClientcontent= (String) msg.obj;
+//                    mClientTextView.setText(mClientcontent);
+//                    break;
+//                case 1:
+//                    String mServerContent= (String) msg.obj;
+//                    mServerTextView.setText(mServerContent);
+//                    break;
+//
+//            }
 //        }
-    }
+//    };
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         isOpen = false;
-<<<<<<< HEAD
-//        mServerThread.isOpen = false;
         if(mServerThread != null){
 //            mServerThread.interrupt();
             mServerThread.stoped();
@@ -279,10 +237,7 @@ public class TcpConnectActivity extends AppCompatActivity {
         if(mClientThread != null){
 //            mClientThread.interrupt();
             mClientThread.stoped();
-=======
-        if(mServerThread != null){
-            mServerThread.interrupt();
->>>>>>> github/master
+
         }
     }
 
